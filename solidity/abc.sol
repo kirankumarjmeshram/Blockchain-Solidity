36fc4c1f76b5077a08deab2afacc83eda5e09c25
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-03-29 12:15:22 +0530

    connected remix vm to vs code successfully

---
+++ solidity/abc.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract local 
+{
+    function store() pure public returns(uint)
+    {
+        uint age = 10;
+        return age;
+    }
+}
+
