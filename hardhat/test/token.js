e57a794545ed045ef775ef221ce2a7819a9c3061
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-05-11 15:48:24 +0530

    hardhat testing the contract

--- hardhat/test/token.js
+++ hardhat/test/token.js
@@ -1,5 +1,17 @@
 const {expect} = require("chai");
 
 describe("Token contract", function () {
-    it('Deployment should assign that total supply of token to the owner'); 
-})
+    it('Deployment should assign that total supply of token to the owner', async function(){
+        const [owner] = await ethers.getSigners();
+        console.log("signers object: ", owner);
+
+        const Token = await ethers.getContractFactory("Token"); // creating contract instance
+
+        const hardhatToken = await Token.deploy();// deploying contract
+
+        const ownerBalance = await hardhatToken.balanceOf(owner.address); // owner balance 10,000
+        console.log("Owner Address:", owner.address);
+
+        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);// total supply 10,000 if both true show+ve result else error
+    }); 
+});
