32ccf97068af1ac7439caf5a6864cf9df3f5461b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-05-16 15:02:31 +0530

    getLatNBitsUsingMask

---
+++ Solidity By Example/bitwiseOperator/getLastNBits.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract getLastNBits {
+    // Get Last n bits from x
+    function getLatNBitsUsingMask(uint x, uint n) external pure returns (uint) {
+        //ex, last 3 bits, ie. n=3;
+        // x      = 1101 = 13
+        // mast   = 0111 = 7 ie. 1<<3 = 0111
+        // x&mask = 0101 = 5;
+        uint mask = (1<<n) -1;
+        return x&mask;
+    }
+}
