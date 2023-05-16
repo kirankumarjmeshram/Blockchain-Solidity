206e43c54d562e9612f5f97320c58d1021cd2900
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-05-16 17:58:10 +0530

    some folder changes

--- Solidity By Example/basic/Counter.sol
+++ Solidity By Example/basic/Counter.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
-
+// constant variables are evaluated at compile-time and 
+//can be of any type, while immutable variables are assigned at
+// deployment-time and can only be value types. Accessing constant variables is gas-free,
+// whereas accessing immutable variables consumes gas.
 contract Counter {
     uint public count ;
     function get() public view returns (uint) {
