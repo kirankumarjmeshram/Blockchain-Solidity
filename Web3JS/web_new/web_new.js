2b614530ac7a4a7de758fd0eea09d53c5409bc12
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-05-29 19:01:49 +0530

    chai app

--- Web3JS/web_new/web_new.js
+++ Web3JS/web_new/web_new.js
@@ -9,20 +9,20 @@ let fileContent = fs.readFileSync("demo.sol").toString();
 console.log(fileContent);
 
 var input = {
-    language : "Solidity",
-    sources: {
-        "demo.sol":{
-            content: fileContent,
-        },
+  language: "Solidity",
+  sources: {
+    "demo.sol": {
+      content: fileContent,
     },
-    
-    settings: {
-        outputSelection: {
-            "*": {
-                "*":["*"],
-            },
-        },
+  },
+
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
     },
+  },
 };
 
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
@@ -31,12 +31,28 @@ console.log(output);
 ABI = output.contracts["demo.sol"]["demo"].abi;
 bytecode = output.contracts["demo.sol"]["demo"].evm.bytecode.object;
 
-console.log("abi : ",ABI);
-console.log("bytecode : ",bytecode);
+console.log("abi : ", ABI);
+console.log("bytecode : ", bytecode);
 
-contract = new web3.eth.Contract(ABI);
+let contract = new web3.eth.Contract(ABI);
 
-// will show all accounts of ganache
-web3.eth.getAccounts().then((accounts)=>{
-    console.log("Accounts : ", accounts);
-})
+let defaultAccount;
+
+web3.eth.getAccounts().then((accounts) => {
+  // will show all accounts of ganache
+  console.log("Accounts : ", accounts);
+  defaultAccount = accounts[0];
+  console.log("Default Account:", defaultAccount); //to deploy the contract from default Account
+  contract
+    .deploy({ data: bytecode })
+    .send({ from: defaultAccount, gas: 470000 })
+    .once("receipt", (receipt) => {
+      //event,transactions,contract address will be returned by blockchain
+      console.log("Contract Address:", receipt.contractAddress);
+    })
+    .then((demoContract) => {
+      demoContract.methods.x().call((err, data) => {
+        console.log("Initial Value:", data);
+      });
+    });
+});
